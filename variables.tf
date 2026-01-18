e644919374a4525ce96e28838da6a3dcf8c49ab8
khouloudchebboubi22-ui <khouloudchebboubi22@gmail.com>
2026-01-18 21:51:54 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,72 +1,28 @@
-# variables.tf
 variable "aws_region" {
   description = "AWS region"
-  type        = string
-  default     = "eu-central-1"
+  type = string
+  default = "eu-central-1"
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+  description = "VPC CIDR block"
+  type = string
+  default = "10.0.0.0/16"
 }
 
-variable "public_subnets" {
-  description = "List of public subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type = string
 }
 
-variable "private_subnets" {
-  description = "List of private subnets"
-  type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-}
-
-variable "availability_zones" {
-  description = "Availability zones"
-  type        = list(string)
-  default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+variable "ami_id" {
+  description = "AMI ID for EC2"
+  type = string
+  default = "ami-0dcc0ebde7b2e00db" # Amazon Linux 2 in Frankfurt
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
-  type        = string
-  default     = "t3.small"
-}
-
-variable "key_name" {
-  description = "SSH key pair name"
-  type        = string
-  default     = "web-hosting-key"
-}
-
-variable "ami_id" {
-  description = "AMI ID for EC2 instances"
-  type        = string
-  default     = "ami-0dcc0ebde7b2e00db" # Amazon Linux 2 in eu-central-1
-}
-
-variable "bucket_name" {
-  description = "S3 bucket name for static website"
-  type        = string
-  default     = "my-static-website-bucket-102303255"
-}
-
-variable "certificate_arn" {
-  description = "ACM certificate ARN for HTTPS"
-  type        = string
-  default     = ""
-}
-
-variable "route53_zone_id" {
-  description = "Route53 hosted zone ID"
-  type        = string
-  default     = ""
-}
-
-variable "domain_name" {
-  description = "Domain name for website"
-  type        = string
-  default     = "example.com"
+  type = string
+  default = "t3.micro"
 }

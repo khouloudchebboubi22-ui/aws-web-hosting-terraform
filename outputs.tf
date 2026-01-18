a104edd6ea36ee244b707338e33afa483280aadf
khouloudchebboubi22-ui <khouloudchebboubi22@gmail.com>
2026-01-18 21:24:03 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,63 +1,19 @@
-output "vpc_id" {
-  description = "ID of the created VPC"
-  value       = aws_vpc.web_vpc.id
+output "website_url" {
+  description = "URL of the deployed website"
+  value       = "https://${module.dns.website_fqdn}"
 }
 
-output "web_server_public_ip" {
-  description = "Public IP of the web server"
-  value       = aws_instance.web_server.public_ip
-}
-
-output "web_server_elastic_ip" {
-  description = "Elastic IP assigned to web server"
-  value       = aws_eip.web_eip.public_ip
+output "cloudfront_distribution_id" {
+  description = "CloudFront distribution ID"
+  value       = module.storage.cloudfront_distribution_id
 }
 
 output "s3_bucket_name" {
-  description = "Name of the S3 bucket for static content"
-  value       = aws_s3_bucket.static_website.bucket
+  description = "S3 bucket name"
+  value       = module.storage.s3_bucket_name
 }
 
-output "s3_website_url" {
-  description = "URL of the S3 static website"
-  value       = "http://${aws_s3_bucket.static_website.bucket}.s3-website.${var.aws_region}.amazonaws.com"
-}
-
-output "ec2_instance_url" {
-  description = "URL to access EC2 web server"
-  value       = "http://${aws_instance.web_server.public_ip}"
-}
-
-output "deployment_success" {
-  description = "Deployment status message"
-  value       = <<EOT
-  ✅ AWS WEB HOSTING DEPLOYMENT SUCCESSFUL!
-  
-  STUDENT: Khouloud Chebboubi
-  MATRICULATION: 102303255
-  COURSE: DLBSEPCP01_E
-  
-  RESOURCES DEPLOYED:
-  1. VPC: ${aws_vpc.web_vpc.id}
-  2. EC2 Web Server: ${aws_instance.web_server.id}
-  3. S3 Static Bucket: ${aws_s3_bucket.static_website.bucket}
-  4. Security Group: ${aws_security_group.web_sg.id}
-  
-  ACCESS POINTS:
-  • EC2 Server: http://${aws_instance.web_server.public_ip}
-  • S3 Website: http://${aws_s3_bucket.static_website.bucket}.s3-website.${var.aws_region}.amazonaws.com
-  
-  TEST COMMANDS FOR TUTOR:
-  $ terraform init
-  $ terraform validate
-  $ terraform plan
-  $ terraform apply -auto-approve
-  
-  This deployment meets all requirements:
-  ✓ Highly Available (multi-AZ ready)
-  ✓ Scalable (Auto Scaling Group pattern)
-  ✓ Secure (Security Groups, IAM)
-  ✓ Global Access (public IP + S3)
-  ✓ Infrastructure as Code (Terraform)
-  EOT
+output "load_balancer_dns" {
+  description = "Application Load Balancer DNS"
+  value       = module.compute.alb_dns_name
 }

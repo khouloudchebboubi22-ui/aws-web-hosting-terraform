521eefebd4330081354ac50b3fc68533df15fbc9
khouloudchebboubi22-ui <khouloudchebboubi22@gmail.com>
2026-01-18 21:42:47 +0100

    Delete modules directory

--- modules/dns/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "website_fqdn" {
-  description = "Fully qualified domain name of website"
-  value       = var.route53_zone_id != "" ? var.domain_name : var.cloudfront_domain_name
-}
--- modules/security/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "waf_acl_arn" {
-  description = "WAF Web ACL ARN"
-  value       = aws_wafv2_web_acl.web.arn
-}
-
-output "ec2_iam_role_arn" {
-  description = "IAM Role ARN for EC2"
-  value       = aws_iam_role.ec2_role.arn
-}

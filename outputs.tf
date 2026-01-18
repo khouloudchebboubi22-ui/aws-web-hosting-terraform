18a850ff8d22f8fcb4266d034d4cc3099da15fa1
khouloudchebboubi22-ui <khouloudchebboubi22@gmail.com>
2026-01-18 21:52:10 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,14 @@
-# outputs.tf
-output "vpc_id" {
-  description = "VPC ID"
-  value       = module.vpc.vpc_id
-}
-
-output "alb_dns_name" {
-  description = "Application Load Balancer DNS name"
-  value       = module.compute.alb_dns_name
-}
-
-output "cloudfront_url" {
-  description = "CloudFront distribution URL"
-  value       = module.cdn.cloudfront_url
+output "website_url" {
+  description = "CloudFront URL"
+  value = aws_cloudfront_distribution.website.domain_name
 }
 
 output "s3_bucket_name" {
   description = "S3 bucket name"
-  value       = module.storage.bucket_name
+  value = aws_s3_bucket.website.bucket
 }
 
-output "website_url" {
-  description = "Final website URL"
-  value       = "https://${var.domain_name}"
-}
-
-output "ec2_instance_ids" {
-  description = "EC2 instance IDs"
-  value       = module.compute.instance_ids
+output "alb_dns_name" {
+  description = "ALB DNS name"
+  value = aws_lb.web.dns_name
 }

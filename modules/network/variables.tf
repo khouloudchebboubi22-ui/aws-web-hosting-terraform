9cc83c68a5a3412608091b9fe0d9a0edb8efdd83
khouloudchebboubi22-ui <khouloudchebboubi22@gmail.com>
2026-01-19 17:54:22 +0100

    Create variables.tf

---
+++ modules/network/variables.tf
@@ -0,0 +1,26 @@
+# modules/network/variables.tf
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "Availability zones for subnets"
+  type        = list(string)
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "testing"
+}

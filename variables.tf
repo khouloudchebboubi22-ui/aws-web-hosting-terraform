2e069757160153c91431e3bd136edf3d1f95863c
khouloudchebboubi22-ui <khouloudchebboubi22@gmail.com>
2026-01-19 18:18:32 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,67 +1,56 @@
 # variables.tf
+
 variable "aws_region" {
-  description = "AWS region to deploy resources"
-  type        = string
+  description = "AWS region"
   default     = "eu-central-1"
 }
 
-variable "environment" {
-  description = "Environment name"
-  type        = string
-  default     = "testing"
-}
-
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
+  description = "CIDR block for VPC"
   default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
   description = "CIDR blocks for public subnets"
-  type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets"
-  type        = list(string)
-  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
 variable "availability_zones" {
-  description = "Availability zones for subnets"
-  type        = list(string)
+  description = "Availability zones"
   default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
 }
 
 variable "bucket_name" {
-  description = "Name of the S3 bucket for static website"
-  type        = string
-  default     = "web-static-content-2025-khouloud"
+  description = "S3 bucket name (must be globally unique)"
+  default     = "my-static-website-bucket-12345"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
-  type        = string
   default     = "t3.small"
 }
 
 variable "key_name" {
-  description = "Name of the EC2 key pair"
-  type        = string
-  default     = "web-hosting-key"
+  description = "SSH key pair name"
+  default     = "my-key-pair"
 }
 
-# Optional variables for DNS
-variable "domain_name" {
-  description = "Domain name for the website"
-  type        = string
-  default     = ""
+variable "min_size" {
+  description = "Min instances in ASG"
+  default     = 2
 }
 
-variable "route53_zone_id" {
-  description = "Route53 zone ID"
-  type        = string
-  default     = ""
+variable "max_size" {
+  description = "Max instances in ASG"
+  default     = 6
+}
+
+variable "desired_capacity" {
+  description = "Desired instances in ASG"
+  default     = 3
 }

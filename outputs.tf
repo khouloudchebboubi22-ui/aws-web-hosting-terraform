a7ddcab0cf7b6df3e2a8452ed9370b70731b95fb
khouloudchebboubi22-ui <khouloudchebboubi22@gmail.com>
2026-01-19 18:19:25 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,55 +1,21 @@
 # outputs.tf
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = module.network.vpc_id
-}
 
-output "public_subnet_ids" {
-  description = "IDs of public subnets"
-  value       = module.network.public_subnet_ids
-}
-
-output "private_subnet_ids" {
-  description = "IDs of private subnets"
-  value       = module.network.private_subnet_ids
-}
-
-output "s3_bucket_name" {
-  description = "Name of the S3 bucket"
-  value       = module.storage.s3_bucket_name
-}
-
-output "s3_bucket_website_endpoint" {
-  description = "Website endpoint of the S3 bucket"
-  value       = module.storage.s3_bucket_website_endpoint
-}
-
-output "cloudfront_distribution_id" {
-  description = "ID of the CloudFront distribution"
-  value       = module.cdn.cloudfront_distribution_id
-}
-
-output "cloudfront_domain_name" {
-  description = "Domain name of the CloudFront distribution"
-  value       = module.cdn.cloudfront_domain_name
-}
-
-output "alb_dns_name" {
-  description = "DNS name of the Application Load Balancer"
-  value       = module.compute.alb_dns_name
+output "cloudfront_domain" {
+  description = "CloudFront distribution domain name"
+  value       = module.s3_cloudfront.cloudfront_domain
 }
 
 output "website_url" {
-  description = "Website URL via CloudFront"
-  value       = "https://${module.cdn.cloudfront_domain_name}"
+  description = "S3 static website URL"
+  value       = module.s3_cloudfront.website_url
 }
 
-output "ec2_instance_ids" {
-  description = "IDs of EC2 instances"
-  value       = module.compute.ec2_instance_ids
+output "alb_dns_name" {
+  description = "ALB DNS name"
+  value       = module.ec2.alb_dns_name
 }
 
-output "auto_scaling_group_name" {
-  description = "Name of the Auto Scaling Group"
-  value       = module.compute.auto_scaling_group_name
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
 }
